9fe76f9d7fe86640c22727122e806071c5c02984
Gustibimo <gstbimo@gmail.com>
2019-06-22 01:15:22 +0200

    update layout and allow external api call

--- app/src/main/java/com/gustibimo/shoppingcart/MainActivity.kt
+++ app/src/main/java/com/gustibimo/shoppingcart/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.gustibimo.shoppingcart
 
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.util.Log
+import android.widget.Toast
+import com.gustibimo.shoppingcart.Adapter.ProductAdapter
+import com.gustibimo.shoppingcart.Services.ApiService
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var apiService: ApiService
+    private lateinit var productAdapter: ProductAdapter
+
+    private var products = listOf<Product>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        swipeRefreshLayout.setColorSchemeColors(ContextCompat.getColor(this, R.color.colorPrimary))
+        swipeRefreshLayout.isRefreshing = true
+
+        products_recyclerview.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+
+        getProducts()
+
+    }
+
+    fun getProducts() {
+        apiService.getProducts().enqueue(object : Callback<List<Product>> {
+
+            override fun onFailure(call: Call<List<Product>>, t: Throwable) {
+                print(t.message)
+                Log.d("Data error", t.message)
+                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
+
+                swipeRefreshLayout.isRefreshing = false
+                products = response.body()!!
+
+                productAdapter = ProductAdapter(this@MainActivity, products)
+
+                products_recyclerview.adapter = productAdapter
+                productAdapter.notifyDataSetChanged()
+
+            }
+
+        })
     }
 }
