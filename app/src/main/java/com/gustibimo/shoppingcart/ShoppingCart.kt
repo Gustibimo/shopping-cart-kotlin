05cff4a97aa68b580b2801b7e59c3a0aacc8c77f
Gustibimo <gstbimo@gmail.com>
2019-06-22 13:41:08 +0200

    Add shoppingCart funcionality

--- app/src/main/java/com/gustibimo/shoppingcart/ShoppingCart.kt
+++ app/src/main/java/com/gustibimo/shoppingcart/ShoppingCart.kt
@@ -1,4 +1,57 @@
 package com.gustibimo.shoppingcart
 
+import android.content.Context
+import io.paperdb.Paper
+
 class ShoppingCart {
+
+    companion object {
+        fun addItem(cartItem: CartItem) {
+            val cart = ShoppingCart.getCart()
+
+            val targetItem = cart.singleOrNull { it.product.id == cartItem.product.id }
+            if (targetItem == null) {
+                cartItem.quantity++
+                cart.add(cartItem)
+            } else {
+                targetItem.quantity++
+            }
+            ShoppingCart.saveCart(cart)
+
+        }
+
+        fun saveCart(cart: MutableList<CartItem>) {
+            Paper.book().write("cart", cart)
+        }
+
+        fun removeItem(cartItem: CartItem, context: Context) {
+            val cart = ShoppingCart.getCart()
+
+            val targetItem = cart.singleOrNull { it.product.id == cartItem.product.id }
+            if (targetItem != null) {
+                if (targetItem.quantity > 0) {
+                    targetItem.quantity--
+                } else {
+                    cart.remove(targetItem)
+                }
+            }
+            ShoppingCart.saveCart(cart)
+        }
+
+        fun getCart(): MutableList<CartItem> {
+            return Paper.book().read("cart", mutableListOf())
+        }
+
+        fun getShoppingCartSize(): Int {
+            var cartSize = 0
+            ShoppingCart.getCart().forEach {
+                cartSize += it.quantity
+            }
+
+            return cartSize
+        }
+
+
+    }
+
 }
